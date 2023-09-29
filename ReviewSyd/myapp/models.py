b5ad41435bcfa841fa4d30cf7a55b92a9fbfb153
Dylan Ledermann <dled5049@uni.sydney.edu.au>
2023-09-29 12:48:53 +1000

    Locations model created
    Added the characteristics for the location model

--- ReviewSyd/myapp/models.py
+++ ReviewSyd/myapp/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Locations(models.Model):
-    name= models.CharField(max_length=200)
-    location= models.CharField(max_length=200)
-    hours=models.CharField(max_length=200)
-    website=models.CharField(max_length=200, blank=True, default='')
-    phone_number=models.CharField(max_length=200, blank=True, default='')
+    name= models.CharField(max_length=255)
+    location= models.CharField(max_length=255)
+    hours=models.CharField(max_length=255)
+    website=models.CharField(max_length=255, blank=True)
+    phone_number=models.CharField(max_length=255, blank=True)
+    amenities=models.CharField(max_length=255, blank=True)
+    avgNoise=models.IntegerField(blank=True, default=0)
+    avgAmen=models.IntegerField(blank=True, default=0)
+    avgClean=models.IntegerField(blank=True, default=0)
+    nearPlaces=models.CharField(max_length=255, blank=True)
 
 class Faq(models.Model):
     question = models.CharField(max_length=250)
     answer = models.CharField(max_length=1000)
+
+class Reviews(models.Model):
+    name=models.CharField(max_length=255)
     

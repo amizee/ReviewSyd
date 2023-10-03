cf1c4c14f42634383004e5e37573f3dc9ed7df37
shwu7827 <shwu7827@uni.sydney.edu.au>
2023-10-03 15:37:38 +1100

    Update views.py

--- ReviewSyd/myapp/views.py
+++ ReviewSyd/myapp/views.py
@@ -5,19 +5,28 @@ from .models import Locations, Faq, Tutor
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
 def ts(request):
     return render(request, "ts.html")
+
+
 def help(request):
     return render(request, "help.html")
+
+
 def privacy(request):
     return render(request, "privacy.html")
+
+
 def feedback(request):
     return render(request, "feedback.html")
-def findTutor(request):
-    return render(request, "findTutor.html")
+
+
 def findTutor(request):
     tutors = Tutor.objects.all()  # get all tutor objects
     return render(request, "findTutor.html", {'tutors': tutors})
+
 
 def add_tutor(request):
     if request.method == 'POST':
@@ -39,17 +48,29 @@ def add_tutor(request):
 def locationList(request):
     items = Locations.objects.all()
     return render(request, "locationList.html", {"locations": items})
+
+
 def faq(request):
     faq_items = Faq.objects.all()
     return render(request, "faq.html", {"faq": faq_items})
+
+
 def location(request, loc):
     location=Locations.objects.get(name=loc)
     return render(request, "location.html", {"location":location})
+
+
 def accountSettings(request):
     return render(request, "accountSettings.html")
+
+
 def login(request):
     return render(request, "login.html")
+
+
 def signup(request):
     return render(request, "signup.html")
+
+
 def signupCompletion(request):
     return render(request, "signupCompletion.html")

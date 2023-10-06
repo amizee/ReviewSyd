585a6bd27891a024e99df631c148492fe8f96d5e
Jerome Ayad <jaya9137@uni.sydney.edu.au>
2023-10-06 14:44:51 +1100

    added changes to location page

--- ReviewSyd/myapp/admin.py
+++ ReviewSyd/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Locations, Faq, Tutor
+from .models import Locations, Faq, Tutor, LocationReviews
 
 # Register your models here.
 #python(3) manage.py makemigrations
@@ -7,3 +7,4 @@ from .models import Locations, Faq, Tutor
 admin.site.register(Locations)
 admin.site.register(Faq)
 admin.site.register(Tutor)
+admin.site.register(LocationReviews)
